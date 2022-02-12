79b902640fdadddf24eacb15804544ac01229744
Chana <64221650+ChanaBaksht@users.noreply.github.com>
2022-02-12 23:23:28 +0200

    end of section 6

--- DatingAppFolder/client/src/app/app.module.ts
+++ DatingAppFolder/client/src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { HttpClientModule } from "@angular/common/http";
 import { NavComponent } from './nav/nav.component'
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
-import { MemberListComponent } from './members/member-list/member-list.component';
-import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
+import { CoreModule } from './modules/core.module';
 
 
 //update commponents:
@@ -24,8 +20,6 @@ import { ListsComponent } from './lists/lists.component';
     NavComponent,
     HomeComponent,
     RegisterComponent,
-    MemberListComponent,
-    MemberDetailComponent,
     MessagesComponent,
     ListsComponent
   ],
@@ -35,8 +29,7 @@ import { ListsComponent } from './lists/lists.component';
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule, 
-    BsDropdownModule.forRoot(),
-
+    CoreModule
   ],
   providers: [],
 
