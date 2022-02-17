ef221cefbd282b9624a70e1aac100a127e5f3c5e
Chana <64221650+ChanaBaksht@users.noreply.github.com>
2022-02-17 23:45:32 +0200

    77. Eexception handling middleware

--- DatingAppFolder/API/DTOs/RegisterDto.cs
+++ DatingAppFolder/API/DTOs/RegisterDto.cs
@@ -1,6 +1,5 @@
 using System.ComponentModel.DataAnnotations;
 
-
 namespace API.DTOs
 {
 
@@ -11,6 +10,7 @@ namespace API.DTOs
         public string Username { get; set; }
 
         [Required]
+        [StringLength(8,MinimumLength =4, ErrorMessage ="You must specify between 4 and 8 chars")]
         public string Password { get; set; }
 
     }
